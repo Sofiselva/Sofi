21fc6c0ac76d9d0b7b4271beeceb83eb3b37e81d
unknown <sofiselva34@gmail.com>
2019-09-18 22:03:49 +0530

    Code committed successfully

---
+++ src/org/test/Sample.java
@@ -0,0 +1,41 @@
+package org.test;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+	
+	public static void main(String[] args) throws AWTException {
+		
+	
+	System.setProperty("webdriver.chrome.driver", "C:\\java\\sofia new\\Selenium\\driver\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://www.snapdeal.com/");
+	
+	driver.findElement(By.id("inputValEnter")).sendKeys("iphonex");
+	Robot r = new Robot();
+	r.keyPress(KeyEvent.VK_ENTER);
+	r.keyRelease(KeyEvent.VK_ENTER);
+	
+	List<WebElement> items = driver.findElements(By.xpath("//a[@class='dp-widget-link hashAdded']"));
+	int i = items.size();
+	System.out.println(i);
+	
+	for (WebElement x : items) {
+		
+		System.out.println(x.getText());
+		
+	}
+	}
+	
+
+}
